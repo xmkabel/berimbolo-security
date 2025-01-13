2012a821276d177ca4dfd8747c6e53adb0bff1b6
xmkabel <mkabel190@gmail.com>
2025-01-13 03:18:53 +0200

    Testimonials Component

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,44 @@
+import react from "react";
+import { Link } from "react-router";
+import './Testimonials.css'
+function Testimonials(){
+    return(
+        <section class="testimonials" id="reviews">
+        <h2>Testimonials</h2>
+        <div class="testimonials-grid">
+            <div class="testimonial-card">
+                <p>"Berimbolo Security provides cutting-edge technology and unmatched professionalism, ensuring complete peace of mind."</p>
+                <div class="testimonial-author">
+                    <img src={require('./Sara.png')} alt="Sara Ahmed Avatar" class="avatar"/>
+                    <div>
+                        <h4>Sara Ahmed</h4>
+                        <p>@saraahmed00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-card">
+                <p>"Surely Recommended! Advanced tech, seamless protection. Sleep easy knowing your world is secured like never before."</p>
+                <div class="testimonial-author">
+                    <img src={require('./Musk.png')} alt="Elon Musk Avatar" class="avatar"/>
+                    <div>
+                    <h4>Elon Musk</h4>
+                    <p>@elonmusk</p>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-card">
+                <p>"Safety reimagined. Berimbolo Security combines innovation and reliability to deliver unparalleled peace of mind."</p>
+                <div class="testimonial-author">
+                    <img src={require('./Kabel.png')} alt="Mohamed Kabel Avatar" class="avatar"/>
+                    <div>
+                    <h4>Mohamed Kabel</h4>
+                    <p>@mohamedkabel</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Testimonials;
