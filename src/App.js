a4edcb31040ae73354e86d8aceae35c575e25c42
xmkabel <mkabel190@gmail.com>
2025-01-13 03:21:26 +0200

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-
+// Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Components
+import Header from './components/Header/Header'; 
+import Features from './components/Features/Features';
+import ProductSection from './components/Products/Products';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Header/>
+      <Features/>
+      <ProductSection/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+      </Router>
+    </>
   );
 }
 
