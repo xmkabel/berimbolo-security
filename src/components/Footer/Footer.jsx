c64825918b8b93b604a711065bee02760862a79d
xmkabel <mkabel190@gmail.com>
2025-01-13 03:21:18 +0200

    Footer Component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Footer.css";
+import { Link } from "react-router";
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; 2025 Berimbolo Security, Inc. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
